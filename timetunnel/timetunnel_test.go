e310bf32c16c8f31b7795cddc31a4d8e169e2566
8liang <51085082@qq.com>
2024-07-19 15:39:05 +0800

    feat: test minute

--- timetunnel/timetunnel_test.go
+++ timetunnel/timetunnel_test.go
@@ -1,71 +1,87 @@
 package timetunnel
 
 import (
-    "github.com/golang-module/carbon/v2"
-    "github.com/stretchr/testify/assert"
-    "testing"
-    "time"
+	"testing"
+	"time"
+
+	"github.com/golang-module/carbon/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 type stubTimeTunnel struct {
-    touchedAt time.Time
-    Hour      bool
-    Day       bool
-    Week      bool
-    Month     bool
+	touchedAt time.Time
+	Minute    bool
+	Hour      bool
+	Day       bool
+	Week      bool
+	Month     bool
+}
+
+// OnMinutePassed implements Tunnel.
+func (s *stubTimeTunnel) OnMinutePassed() {
+	s.Minute = true
 }
 
 func (s *stubTimeTunnel) GetTouchedAt() time.Time {
-    return s.touchedAt
+	return s.touchedAt
 }
 
 func (s *stubTimeTunnel) SetTouchedAt(t time.Time) {
-    s.touchedAt = t
+	s.touchedAt = t
 }
 
 func (s *stubTimeTunnel) OnHourPassed() {
-    s.Hour = true
+	s.Hour = true
 }
 
 func (s *stubTimeTunnel) OnDayPassed() {
-    s.Day = true
+	s.Day = true
 }
 
 func (s *stubTimeTunnel) OnWeekPassed() {
-    s.Week = true
+	s.Week = true
 }
 
 func (s *stubTimeTunnel) OnMonthPassed() {
-    s.Month = true
+	s.Month = true
 }
 
 func TestTunnel(t *testing.T) {
-    last, _ := time.Parse(time.RFC3339, "2024-03-03T15:02:03.332Z")
-    current, _ := time.Parse(time.RFC3339, "2024-03-03T16:02:03.332Z")
-    tt := &stubTimeTunnel{touchedAt: last}
-    Pass(tt, WithWeekStartsAt(carbon.Monday), WithCurrentTime(current))
-    assert.True(t, tt.Week, "week should be crossed")
-    assert.Equal(t, current.Unix(), tt.GetTouchedAt().Unix(), "touchedAt should be updated")
+	last, _ := time.Parse(time.RFC3339, "2024-03-03T15:02:03.332Z")
+	current, _ := time.Parse(time.RFC3339, "2024-03-03T16:02:03.332Z")
+	tt := &stubTimeTunnel{touchedAt: last}
+	Pass(tt, WithWeekStartsAt(carbon.Monday), WithCurrentTime(current))
+	assert.True(t, tt.Week, "week should be crossed")
+	assert.Equal(t, current.Unix(), tt.GetTouchedAt().Unix(), "touchedAt should be updated")
 
-    tm := time.Now()
-    stub := &stubTimeTunnel{touchedAt: tm.Add(-time.Hour)}
-    Pass(stub, WithWeekStartsAt(carbon.Monday))
-    assert.True(t, stub.Hour, "hour should be crossed")
-    assert.False(t, stub.Day, "day should not be crossed")
-    assert.False(t, stub.Week, "week should not be crossed")
-    assert.False(t, stub.Month, "month should not be crossed")
-    assert.Equal(t, time.Now().Unix(), stub.GetTouchedAt().Unix(), "touchedAt should be updated")
+	tm := time.Now()
+	stub := &stubTimeTunnel{touchedAt: tm.Add(-time.Second * 30)}
+	Pass(stub, WithWeekStartsAt(carbon.Monday))
+	assert.True(t, stub.Minute, "hour should be crossed")
+	assert.False(t, stub.Hour, "hour should not be crossed")
+	assert.False(t, stub.Day, "day should not be crossed")
+	assert.False(t, stub.Week, "week should not be crossed")
+	assert.False(t, stub.Month, "month should not be crossed")
+	assert.Equal(t, time.Now().Unix(), stub.GetTouchedAt().Unix(), "touchedAt should be updated")
 
-    stub = &stubTimeTunnel{touchedAt: tm.Add(-time.Hour * 24)}
-    Pass(stub, WithWeekStartsAt(carbon.Monday))
-    assert.True(t, stub.Hour, "hour should be crossed")
-    assert.True(t, stub.Day, "day should be crossed")
-    assert.False(t, stub.Week, "week should not be crossed")
-    assert.False(t, stub.Month, "month should not be crossed")
+	stub = &stubTimeTunnel{touchedAt: tm.Add(-time.Hour)}
+	Pass(stub, WithWeekStartsAt(carbon.Monday))
+	assert.True(t, stub.Hour, "hour should be crossed")
+	assert.False(t, stub.Day, "day should not be crossed")
+	assert.False(t, stub.Week, "week should not be crossed")
+	assert.False(t, stub.Month, "month should not be crossed")
+	assert.Equal(t, time.Now().Unix(), stub.GetTouchedAt().Unix(), "touchedAt should be updated")
 
-    stub = &stubTimeTunnel{touchedAt: tm.Add(-time.Hour * 24 * 6)}
-    Pass(stub, WithWeekStartsAt(carbon.Monday))
-    assert.True(t, stub.Hour, "hour should be crossed")
-    assert.True(t, stub.Week, "week should be crossed")
-    assert.True(t, stub.Day, "day should be crossed")
+	stub = &stubTimeTunnel{touchedAt: tm.Add(-time.Hour * 24)}
+	Pass(stub, WithWeekStartsAt(carbon.Monday))
+	assert.True(t, stub.Hour, "hour should be crossed")
+	assert.True(t, stub.Day, "day should be crossed")
+	assert.False(t, stub.Week, "week should not be crossed")
+	assert.False(t, stub.Month, "month should not be crossed")
+
+	stub = &stubTimeTunnel{touchedAt: tm.Add(-time.Hour * 24 * 6)}
+	Pass(stub, WithWeekStartsAt(carbon.Monday))
+	assert.True(t, stub.Hour, "hour should be crossed")
+	assert.True(t, stub.Week, "week should be crossed")
+	assert.True(t, stub.Day, "day should be crossed")
 }
