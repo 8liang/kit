094af50208ae9490c6be66ad2981ce1c6e938c55
8liang <51085082@qq.com>
2024-08-02 17:06:59 +0800

    feat: Add Weight for lottery

--- lottery/lottery.go
+++ lottery/lottery.go
@@ -1,57 +1,62 @@
 package lottery
 
 import (
-    "math/rand"
+	"math/rand"
 )
 
 // NewLottery create a new lottery pool
 func NewLottery[T comparable]() *Lottery[T] {
-    var l Lottery[T]
-    return &l
+	var l Lottery[T]
+	return &l
 }
 
 type Lottery[T comparable] struct {
-    total  int
-    weight int
-    pool   []*item[T]
+	total  int
+	weight int
+	pool   []*item[T]
 }
 
 type Roller func(int) int
 
 // AddItem add an item for Lottery draw
 func (l *Lottery[T]) AddItem(ID T, weight int) {
-    l.total++
-    l.weight += weight
-    l.pool = append(l.pool, newItem(ID, weight))
+	l.total++
+	l.weight += weight
+	l.pool = append(l.pool, newItem(ID, weight))
 }
 
 // Draw draw a lottery
 func (l *Lottery[T]) Draw(random ...Roller) T {
-    var roller Roller
-    if len(random) == 0 {
-        roller = rand.Intn
-    } else {
-        roller = random[0]
-        if roller == nil {
-            roller = rand.Intn
-        }
-    }
-    r := roller(l.weight)
-    return l.result(r).ID
+	var roller Roller
+	if len(random) == 0 {
+		roller = rand.Intn
+	} else {
+		roller = random[0]
+		if roller == nil {
+			roller = rand.Intn
+		}
+	}
+	r := roller(l.weight)
+	return l.result(r).ID
 }
 
 // Len get amount of items
 func (l *Lottery[T]) Len() int {
-    return l.total
+	return l.total
+}
+
+// Weight get total weight of items
+func (l *Lottery[T]) Weight() int {
+	return l.weight
 }
 
 func (l *Lottery[T]) result(r int) *item[T] {
-    min := 0
-    for _, v := range l.pool {
-        min += v.Weight
-        if r < min {
-            return v
-        }
-    }
-    return l.pool[0]
+	min := 0
+	for _, v := range l.pool {
+		min += v.Weight
+		if r < min {
+			return v
+		}
+	}
+	return l.pool[0]
 }
